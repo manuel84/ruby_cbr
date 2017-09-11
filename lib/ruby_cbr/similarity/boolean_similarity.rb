ac365c3e60573b80aa720ac8b452e79e27e29b70
Manuel Dudda <manuel.dudda@gmail.com>
2017-09-11 13:42:38 +0200

    boolean fix

--- lib/ruby_cbr/similarity/boolean_similarity.rb
+++ lib/ruby_cbr/similarity/boolean_similarity.rb
@@ -5,7 +5,7 @@ module CBR
       def compare(real_value)
         one = BigDecimal.new('1.0', 4)
         zero = BigDecimal.new('0.0', 4)
-        result = BigDecimal.new(real_value.to_i)
+        result = BigDecimal.new((real_value && 1 || 0))
         result = [result, one].min # max 1
         result = [result, zero].max # min 0
         result
